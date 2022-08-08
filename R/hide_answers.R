23edc1229f8981a9b43bbef3b111c6a1241416a5
Catherine Blatter <catherine.blatter@unibas.ch>
2022-08-08 23:49:36 +0200

    Initial commit

---
+++ R/hide_answers.R
@@ -0,0 +1,31 @@
+#' Hide answer slides in quarto revealjs-presentations
+#'
+#' @param x An .Rmd file
+#' @param ...
+#'
+#' @return a HTML file
+#' @export
+#'
+hide_answer <- function(x, ...){
+
+  quarto::quarto_render(input = x,
+                        ...,
+                        pandoc_args = "--lua-filter=filters/hideanswer.lua")
+
+}
+
+
+#' Show answer slides in quarto revealjs-presentations
+#'
+#' @param x An .Rmd file
+#' @param ...
+#'
+#' @return a HTML file
+#' @export
+#'
+show_answer <- function(x, ...){
+
+  quarto::quarto_render(input = x,
+                        ...)
+
+}
